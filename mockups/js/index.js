030aa3deabc8f8b37503f4da94367739c6393ead
Dewitt Buckingham <DaJoker29@users.noreply.github.com>
2024-06-08 00:40:21 -0700

    Add Interactive Search
    Not functional yet. But the layout and toggling is there.

--- mockups/js/index.js
+++ mockups/js/index.js
@@ -1,3 +1,13 @@
 import menu from './menu.js';
+import search from './search.js';
+
+document.body.classList.remove('no-js');
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    document.body.classList.add('is-complete');
+  }
+};
 
 menu();
+search();
