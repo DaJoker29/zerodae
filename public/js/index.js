78e0a5692efb18edfadd35f962e09726ea6ea10b
Dewitt Buckingham <DaJoker29@users.noreply.github.com>
2024-06-19 11:13:27 -0700

    Style Blog Landing
    - Start adding Light Theme variables.
    - Remove the sidebar
    - Updated Color Palette with Fire Theme variables.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 import intersect from './intersect.js';
 import lightDark from './light-dark.js';
-import masthead from './masthead.js';
 import menu from './menu.js';
 import search from './search.js';
 
@@ -19,6 +18,5 @@ intersect();
 lightDark();
 
 if (document.body.classList.contains('theme-fire')) {
-  masthead();
   search();
 }
