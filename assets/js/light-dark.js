38c5d578ac1115d824cef1a79d2279e4663e0f81
Dewitt Buckingham <DaJoker29@users.noreply.github.com>
2024-11-10 14:30:41 -0800

    Fix bug css overlay causing the blog page to lose all functionality.

--- assets/js/light-dark.js
+++ assets/js/light-dark.js
@@ -1,22 +1,25 @@
 const setTheme = (theme) => {
-  localStorage.setItem('theme', theme);
+  localStorage.setItem("theme", theme);
   document.body.dataset.theme = theme;
 };
 
 const toggleTheme = () => {
   const theme = document.body.dataset.theme;
-  setTheme(theme === 'light' ? 'dark' : 'light');
+  setTheme(theme === "light" ? "dark" : "light");
 };
 
 const lightDark = () => {
-  const toggle = document.querySelector('[data-theme-toggle]');
-  const currentTheme = localStorage.getItem('theme') || prefersDarkMode;
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
-    ? 'dark'
-    : 'light';
+  const toggle = document.querySelector("[data-theme-toggle]");
+  const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)")
+    ? "dark"
+    : "light";
+  const currentTheme =
+    localStorage.getItem("theme") !== null
+      ? localStorage.getItem("theme")
+      : prefersDarkMode;
 
   setTheme(currentTheme);
-  toggle.addEventListener('click', toggleTheme);
+  toggle.addEventListener("click", toggleTheme);
 };
 
 export default lightDark;
